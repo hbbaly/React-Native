0858dc51717f08499d6b733de7ee607fad44ddd2
hbbaly <1422699902@qq.com>
2019-01-16 00:18:41 +0800

    ADD:获取设备的尺寸和系统

--- basic/views/props/Props.js
+++ basic/views/props/Props.js
@@ -1,12 +1,35 @@
 import React from 'react'
-import { Image } from 'react-native'
+import { Image,View,Text, Dimensions,Platform ,StyleSheet} from 'react-native'
 export default class Props extends React.Component {
+  componentDidMount () {
+    
+  }
   render () {
     let pic = {
       uri:'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     }
     return (
-      <Image source={pic} style={{width: '100%', height: 200}} />
+      <View style = {styles.container}>
+        <Image source={pic} style={{width: '100%', height: 200}} />
+        <Text>设备宽度：{Dimensions.get('window').width}</Text>
+        <Text>设备高度：{Dimensions.get('window').height}</Text>
+        <Text>ScreenScale：{Dimensions.get('window').scale}</Text>
+        <Text>{Platform.OS}</Text>
+        <Text>{Platform.Version}</Text>
+      </View>
     )
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    ...Platform.select({
+      ios: {
+        backgroundColor: "red"
+      },
+      android: {
+        backgroundColor: "blue"
+      }
+    })
+  }
+});
