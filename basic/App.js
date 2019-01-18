5a34df29e40f06c34844d7542b2fabe635b35806
hbbaly <1422699902@qq.com>
2019-01-18 20:23:08 +0800

    ADD: 使用react-native-splash-screen设置启动页

--- basic/App.js
+++ basic/App.js
@@ -9,7 +9,11 @@ import Scroll from './views/scroll/scroll'
 import FlatListBasics  from './views/scroll/FlatList'
 import SectionListDemo from './views/scroll/SectionList'
 import FetchDemo from './views/fetch/fetch'
+import SplashScreen from 'react-native-splash-screen'
 export default class App extends Component {
+  componentDidMount() {
+    SplashScreen.hide();
+}
   render() {
     return (
         <ScrollView>
--- basic/android/app/src/main/java/com/basic/MainActivity.java
+++ basic/android/app/src/main/java/com/basic/MainActivity.java
@@ -1,13 +1,18 @@
 package com.basic;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
-
+import org.devio.rn.splashscreen.SplashScreen; // here
 public class MainActivity extends ReactActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, true);  // here
+        super.onCreate(savedInstanceState);
+    }
     @Override
     protected String getMainComponentName() {
         return "basic";
