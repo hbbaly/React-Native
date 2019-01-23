7f45aa1087cbe3f9016a15a089d070bab3a00ab6
hbbaly <1422699902@qq.com>
2019-01-23 14:00:45 +0800

    ADD:自定义navigationBar

--- popular/res/views/modalScreen.js
+++ popular/res/views/modalScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native'
 export default class ModalScreen extends Component{
+  constructor (props) {
+    super (props)
+  }
   render () {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
--- popular/res/views/singin.js
+++ popular/res/views/singin.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native'
+import NavigationBar from './navigationBar'
+
 export default class Singin extends Component{
   render () {
     return (
       <View>
+        <NavigationBar title='登陆页面' />
         <Text>这里是登陆页面</Text>
       </View>
     )
