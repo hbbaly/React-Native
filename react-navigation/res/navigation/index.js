f1d9f19f7cd2cba1e44f0ca14bf36669d835b9fe
hbbaly <1422699902@qq.com>
2019-01-23 23:47:39 +0800

    ADD: headerRight自定义

--- react-navigation/res/navigation/index.js
+++ react-navigation/res/navigation/index.js
@@ -1,23 +1,33 @@
+import React from 'react'
+import { Button } from 'react-native'
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 import Index from '../views/index'
 import Detail from '../views/details'
 const appContainer = createStackNavigator(
   { 
-    Home:{ 
+    Home:{
       screen: Index,
-      navigationOptions:{
-        title: 'Index',
-        headerStyle: {
-          backgroundColor: '#f4511e',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          textAlign: 'center',
+      navigationOptions: ({ navigation }) => {
+        return {
+          title: 'Index',
+          headerRight:(
+            <Button title='detail' onPress = { () =>
+              navigation.navigate('Detail')
+            } />
+          ),
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            textAlign: 'center',
+          }
         }
       }
     },
     Detail:{ 
       screen: Detail,
+      
       navigationOptions: ({navigation}) => {
         return {
           title: navigation.getParam('id','Detail'),
