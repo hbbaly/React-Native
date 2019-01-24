3473e97f723c73ed32ef1296419390d093c0b5de
hbbaly <1422699902@qq.com>
2019-01-24 23:15:30 +0800

    ADD: 打包apk

--- react-navigation/res/views/drawernav.js
+++ react-navigation/res/views/drawernav.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { createDrawerNavigator } from 'react-navigation'
 import { View, Text, Button } from 'react-native'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 class Page1 extends Component {
   render () {
     return (
@@ -12,7 +13,7 @@ class Page1 extends Component {
       <Button title='close drawer' onPress = {() =>
         this.props.navigation.closeDrawer()
       }></Button>
-       <Button title='close drawer' onPress = {() =>
+       <Button title='toggle drawer' onPress = {() =>
         this.props.navigation.toggleDrawer()
       }></Button>
     </View>
@@ -30,7 +31,7 @@ class Page2 extends Component {
       <Button title='close drawer' onPress = {() =>
         this.props.navigation.closeDrawer()
       }></Button>
-       <Button title='close drawer' onPress = {() =>
+       <Button title='toggle drawer' onPress = {() =>
         this.props.navigation.toggleDrawer()
       }></Button>
     </View>
@@ -40,9 +41,27 @@ class Page2 extends Component {
 const MyDrawerNavigator = createDrawerNavigator({
   Page1: {
     screen: Page1,
+    navigationOptions:{
+      drawerLabel:'Page1',
+      drawerIcon: ({tintColor}) => (
+        <MaterialIcons
+            name={"github"}
+            size={24}
+            style={{color: tintColor}}/>
+    )
+    }
   },
   Page2: {
     screen: Page2,
+    navigationOptions:{
+      drawerLabel:'Page1',
+      drawerIcon: ({tintColor}) => (
+        <MaterialIcons
+            name={"app-store"}
+            size={24}
+            style={{color: tintColor}}/>
+    )
+    }
   },
 });
 export default MyDrawerNavigator
